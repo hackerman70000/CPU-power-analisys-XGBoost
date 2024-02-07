847c7ebfe9b1aa5ec0258ea206f1dd0bc66d142a
bdmitruk <01178272@pw.edu.pl>
2024-02-07 22:48:46 +0100

    Error handling.

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -1,10 +1,8 @@
-import glob
 import os
-import resource
-
-import numpy as np
 import pandas as pd
-
+import glob
+import numpy as np
+import psutil
 
 def prepare_dataset(input_folder, output_folder):
     input_path = os.path.join("resources", input_folder)
@@ -21,30 +19,18 @@ def prepare_dataset(input_folder, output_folder):
 
     files = os.listdir(input_path)
 
-    try:
-        # Check RAM usage before processing
-        memory_limit = resource.getrlimit(resource.RLIMIT_AS)[0]
-        current_memory_usage = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss * 1024
-        if current_memory_usage > 0.9 * memory_limit:
-            raise MemoryError("Exceeded 90% of available memory. Aborting.")
-
-        # Data cleansing
-        for i, file_name in enumerate(files):
-            if file_name.endswith(".csv"):
-                df = pd.read_csv(os.path.join(input_path, file_name))
-                df["Encrypting"] = (df['D0'] == 1) & (df['D1'] == 0)
-                df = df.drop(columns=['D0', 'D1', 'D2', 'D3', 'D4', 'D5', 'D6', 'D7'])
-                new_file_name = f"dataset{i + 1}.csv"
-                calculate_averages(df)
-                df = df.dropna()
-                df.to_csv(os.path.join(output_path, new_file_name), index=False)
-        print("Data enrichment has been completed.")
-        print("Data processing has been completed.")
-    except MemoryError as mem_err:
-        print(mem_err)
-    except Exception as e:
-        print("Error occurred during data preparation:", e)
-
+    # Data cleansing
+    for i, file_name in enumerate(files):
+        if file_name.endswith(".csv"):
+            df = pd.read_csv(os.path.join(input_path, file_name))
+            df["Encrypting"] = (df['D0'] == 1) & (df['D1'] == 0)
+            df = df.drop(columns=['D0', 'D1', 'D2', 'D3', 'D4', 'D5', 'D6', 'D7'])
+            new_file_name = f"dataset{i + 1}.csv"
+            calculate_averages(df)
+            df = df.dropna()
+            df.to_csv(os.path.join(output_path, new_file_name), index=False)
+    print("Data enrichment has been completed.")
+    print("Data processing has been completed.")
 
 def calculate_averages(df):
     window_sizes = [3, 5, 7, 10, 15, 20, 50, 100]
@@ -63,7 +49,6 @@ def calculate_averages(df):
         # Exponential Moving Average (EMA)
         ema_col_name = f'EMA_{window_size}'
         df[ema_col_name] = df['Current(uA)'].ewm(span=window_size, adjust=False).mean().round(3)
-
 
 def concatenate_datasets(input_folder, output_folder):
     input_path = os.path.join("resources", input_folder)
